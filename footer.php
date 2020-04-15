9796d38436719c4de0f3638a0bb8bdacacbe990a
dewintanur <63717735+dewintanur@users.noreply.github.com>
2020-04-15 21:48:38 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="http://localhost/siperpus/aset/bootstrap/css/bootstrap.min.css">
+      <link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+    <title>Siperpus</title>  </head>
+  <body>
+      <script src="http://localhost/siperpus/aset/jquery.js"></script>
+      <script src="http://localhost/siperpus/aset/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
